ba63167d7e145e87c0f7414b05e7f796e9f3050a
John Cross <jwcross1@dmacc.edu>
2019-05-31 09:15:03 -0500

    playing with int variable

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,18 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Hello, John!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myNewTotal = 1000 - myTotal;
+
+        System.out.println(myNewTotal);
+
 
     }
 }
